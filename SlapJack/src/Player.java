2e74368739b64f6773674c466b4fe60e4f34512e
unknown <bkprovow@mail.weber.edu>
2015-10-07 08:17:39 -0600

    stub for player

--- SlapJack/src/Player.java
+++ SlapJack/src/Player.java
@@ -1,4 +1,18 @@
+import java.util.ArrayList;
+import java.util.List;
 
 public class Player {
-
+	private List<Card> cards = new ArrayList<>();
+	
+	public void slap() {
+		
+	}
+	
+	public void addCardToHand(Card card) {
+		
+	}
+	
+	public Card drawCard() {
+		return null;
+	}
 }
