9ac529681c21e2961a06262e859ae63a3bb184d5
CNew88 <calebnew@mail.weber.edu>
2015-10-07 08:09:58 -0600

    Updates to Card and Deck to work better together now.

--- SlapJack/src/Card.java
+++ SlapJack/src/Card.java
@@ -1,25 +1,27 @@
+import java.awt.image.*;
+
 public class Card {
 
-    public enum Rank {ACE, DEUCE, THREE, FOUR, FIVE,
+   public enum Rank {ACE, DEUCE, THREE, FOUR, FIVE,
 		SIX, SEVEN, EIGHT, NINE, TEN, JACK, QUEEN,
 		KING
 	}
+   
    public enum Suit { CLUB, SPADE, HEART, DIAMOND }
    
    
-   public Card(int suit, int rank, image i) {
-		this.image = i;
-		this.rank = Rank.values()[rank];
-		this.suit = Suit.values()[suit];
-   }
+   public Card(Rank r, Suit s, BufferedImage img) {
+        rank = r;
+        suit = s;
+        image = img;
+    }
    
     
    
    private Rank rank;
 	private Suit suit;
    private final BufferedImage image;
-   
-   public int value() { return rank.value; }
+
    public Rank rank() { return rank; }
    public Suit suit() { return suit; }
    public BufferedImage image() { return image; }
--- SlapJack/src/Deck.java
+++ SlapJack/src/Deck.java
@@ -1,22 +1,56 @@
 import java.util.ArrayList;
 import java.util.Collections;
+import java.awt.event.*;
+import java.awt.image.*;
+import java.net.*;
+import java.io.IOException;
+import java.util.List;
+import java.io.File;
+import javax.imageio.*;
 
 public class Deck {
-    private ArrayList<Card> cards;
+    private List<Card> cards = new ArrayList<>();
 
     public Deck() {
-        cards = new ArrayList<>();
-        for (int i = 0; i > 52; i++) {
-        	cards.add(new Card(i));
-        }
-    }
+      try {
+            cards = createDeck();
+         } catch (MalformedURLException e) {
+            e.printStackTrace();
+            System.exit(-1);
+         } catch (IOException e) {
+            e.printStackTrace();
+            System.exit(-1);
+         }
+      this.shuffle();   
+	 }
 
     public void shuffle() {
         Collections.shuffle(cards);
     }
 
-    public ArrayList<Card> dealCards() {
-		return cards;
-    	
-    }
+    
+    static public List<Card> createDeck() throws MalformedURLException, IOException {
+            BufferedImage spriteSheet = ImageIO.read(new File("spritesheet.png"));
+            int width = spriteSheet.getWidth();
+            int height = spriteSheet.getHeight();
+            List<Card> deck = new ArrayList<>();
+            int suit = 0;
+            int rank = 0;
+            for (Card.Suit _suit : Card.Suit.values()) {
+               for (Card.Rank _rank : Card.Rank.values()) {
+                  int x = (rank * width) / 13;
+                  int y = (suit * height) / 4;
+                  int w = width / 13;
+                  int h = height / 4;
+                  
+                  BufferedImage cardImg = spriteSheet.getSubimage(x, y, w, h);
+                  deck.add(new Card(_rank, _suit, cardImg));
+                  rank++;
+               }
+               suit++;
+               rank = 0;
+            }
+            Collections.shuffle(deck);
+            return deck;
+         }
 }
