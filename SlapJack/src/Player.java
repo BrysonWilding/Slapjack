13bd4593c3d62ac0ec23f52a79195b283c8a6c0f
unknown <bkprovow@mail.weber.edu>
2015-10-07 08:19:27 -0600

    drawcard and add card

--- SlapJack/src/Player.java
+++ SlapJack/src/Player.java
@@ -2,17 +2,19 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Player {
-	private List<Card> cards = new ArrayList<>();
+	private List<Card> hand = new ArrayList<>();
 	
 	public void slap() {
 		
 	}
 	
 	public void addCardToHand(Card card) {
-		
+		hand.add(card);
 	}
 	
 	public Card drawCard() {
-		return null;
+		Card cardToPlay = hand.get(0);
+		hand.remove(0);
+		return cardToPlay;
 	}
 }
