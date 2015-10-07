bf6c1cc3c5ce0169d741aeb4dade9bd19584a711
CNew88 <calebnew@mail.weber.edu>
2015-10-07 07:57:27 -0600

    Update to Card.java

--- SlapJack/src/Card.java
+++ SlapJack/src/Card.java
@@ -1,11 +1,28 @@
 public class Card {
-    private int cardNumber;
 
-    public Card(int cardNumber) {
-        this.cardNumber = cardNumber;
-    }
-
-    public int getCard() {
-        return cardNumber;
-    }
+    public enum Rank {ACE, DEUCE, THREE, FOUR, FIVE,
+		SIX, SEVEN, EIGHT, NINE, TEN, JACK, QUEEN,
+		KING
+	}
+   public enum Suit { CLUB, SPADE, HEART, DIAMOND }
+   
+   
+   public Card(int suit, int rank, image i) {
+		this.image = i;
+		this.rank = Rank.values()[rank];
+		this.suit = Suit.values()[suit];
+   }
+   
+    
+   
+   private Rank rank;
+	private Suit suit;
+   private final BufferedImage image;
+   
+   public int value() { return rank.value; }
+   public Rank rank() { return rank; }
+   public Suit suit() { return suit; }
+   public BufferedImage image() { return image; }
+   public String toString() { return rank + " of " + suit; }
+    
 }
