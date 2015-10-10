5bc685ff36d89b4d45d455bcb83f208eefb68045
Rachael <silverwoodra@gmail.com>
2015-10-10 10:23:06 -0600

    minor bug fixes

--- SlapJack/src/Board.java
+++ SlapJack/src/Board.java
@@ -16,19 +16,13 @@ public class Board extends JFrame {
        setContentPane(new JLabel(new ImageIcon(imageFile.getAbsolutePath())));
        initComponents();
 	}
-   
-   Board(){
-      // add color here
-      initComponents();
-   }
-   
+
    private void initComponents(){
       setTitle("Slapjack");
 
       Deck d = new Deck();
       player.addCardToHand(d.getCards().subList(0,26));
       cpu.addCardToHand(d.getCards().subList(26, 52));
-
 
       //DELETE THIS LINE.  Puts cards in center deck for testing purposes
       cards = player.getHand();
@@ -69,12 +63,11 @@ public class Board extends JFrame {
       public void paintComponent(Graphics g){
          super.paintComponent(g);
          int h = getWidth()/2-71;
-         g.drawLine(getWidth()/2, 10, getWidth()/2, 500);
 
          //displays the cpu card pile until it has no more cards
          int numCpuCards = cpu.getHand().size(); //get number of cpu cards
          if(numCpuCards > 0) {
-            g.drawImage(back, h, 100, 143, 200, getContentPane()); //display the back of cpu card
+            g.drawImage(back, h, 100, 143, 200, this); //display the back of cpu card
             Polygon cpuShadow = new Polygon();
             cpuShadow.addPoint(h+ 143, 100);
             cpuShadow.addPoint(h+ 143, 300);
@@ -89,13 +82,14 @@ public class Board extends JFrame {
          //displays the middle deck until there is no more cards
          int numDeckCards = cards.size(); //get number of deck cards
          if(numDeckCards > 0) {
-            int y = (getHeight() - 250)/2; //find where card is located on JPanel
+            int y = (getHeight() - 250)/2; //where card is located on JPanel
 
             //displays the most recently flipped card
-            g.drawImage(cards.get(cards.size() - 1).getImage(), h, y, 143, 200, getContentPane());
+            g.drawImage(cards.get(cards.size() - 1).getImage(), h, y, 143, 200, this);
             Polygon deckShadow = new Polygon();
             deckShadow.addPoint(h+143, y);
-            deckShadow.addPoint(h+143, y + 200);
+            deckShadow.addPoint(h+143, y +192);
+            deckShadow.addPoint(h+135, y+200);
             deckShadow.addPoint(h, y + 200);
             deckShadow.addPoint(h+10 + numDeckCards / 5, y + 200 + numDeckCards / 3);
             deckShadow.addPoint(h+143 + numDeckCards / 3, y + 200 + numDeckCards / 3);
@@ -109,7 +103,7 @@ public class Board extends JFrame {
          int numPlayerCards = player.getHand().size(); //get number of player cards
          if(numPlayerCards > 0) {
             int y = getHeight()-350; //find where card is located on JPanel
-            g.drawImage(back, h, y, 143, 200, getContentPane()); //display back of player card
+            g.drawImage(back, h, y, 143, 200, this); //display back of player card
             Polygon playerShadow = new Polygon();
             playerShadow.addPoint(h+143, y);
             playerShadow.addPoint(h+143, y + 200);
@@ -121,6 +115,9 @@ public class Board extends JFrame {
             g.fillPolygon(playerShadow); //draw shadow
          }
 
+         g.setFont(new Font(Font.SERIF, Font.PLAIN, 20));
+         g.drawString(Integer.toString(cpu.getHand().size()), getWidth()/2-5, 90);
+         g.drawString(Integer.toString(player.getHand().size()), getWidth()/2-5, getHeight()-115);
       }
 
    }
